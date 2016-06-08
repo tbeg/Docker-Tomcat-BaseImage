eda8fe3b68c9053dbbc50ecf6457cc8d8b346b49
Michael Nesslinger <Michael.Nesslinger@traveltainment.de>
2016-06-08 13:41:53 +0200

    Added support for proxy settings: apt, env varibales, curl Added logstash

--- DockerFiles/configuration/Fix_Proxy_Settings.sh
+++ DockerFiles/configuration/Fix_Proxy_Settings.sh
@@ -11,23 +11,22 @@
 
 if [ -z "${DOCKER_CONTAINER_PROXY_PROTOCOL}" ] && [ -z "${DOCKER_CONTAINER_PROXY_HOST}" ] && [ -z "${DOCKER_CONTAINER_PROXY_PORT}" ];
 then
-echo "Setting up proxy configurations for container...";
-
-# 1. Setting environment variables
-echo "Setting up proxy environment variables...";
-export http_proxy="${DOCKER_CONTAINER_PROXY_PROTOCOL}://${DOCKER_CONTAINER_PROXY_HOST}:${DOCKER_CONTAINER_PROXY_PORT}"
-export https_proxy="${DOCKER_CONTAINER_PROXY_PROTOCOL}://${DOCKER_CONTAINER_PROXY_HOST}:${DOCKER_CONTAINER_PROXY_PORT}"
-export HTTP_PROXY="${DOCKER_CONTAINER_PROXY_PROTOCOL}://${DOCKER_CONTAINER_PROXY_HOST}:${DOCKER_CONTAINER_PROXY_PORT}"
-export HTTPS_PROXY="${DOCKER_CONTAINER_PROXY_PROTOCOL}://${DOCKER_CONTAINER_PROXY_HOST}:${DOCKER_CONTAINER_PROXY_PORT}"
-
-# 2. Setting proxy for apt
-echo "Setting up proxy for apt ...";
-envsubst "$DOCKER_CONTAINER_PROXY_PROTOCOL:$DOCKER_CONTAINER_PROXY_HOST:$DOCKER_CONTAINER_PROXY_PORT" < /tmp/apt_template.conf > /etc/apt/apt.conf
-
-# 3. Setting proxy for curl
-echo "Setting up proxy for curl ...";
-envsubst "$DOCKER_CONTAINER_PROXY_PROTOCOL:$DOCKER_CONTAINER_PROXY_HOST:$DOCKER_CONTAINER_PROXY_PORT" < /tmp/.curlrc_template > ~/.curlrc
-
-else
     echo "Skipping proxy configuration for container...";
+else
+    echo "Setting up proxy configurations for container...";
+
+    # 1. Setting environment variables
+    echo "Setting up proxy environment variables...";
+    export http_proxy="${DOCKER_CONTAINER_PROXY_PROTOCOL}://${DOCKER_CONTAINER_PROXY_HOST}:${DOCKER_CONTAINER_PROXY_PORT}"
+    export https_proxy="${DOCKER_CONTAINER_PROXY_PROTOCOL}://${DOCKER_CONTAINER_PROXY_HOST}:${DOCKER_CONTAINER_PROXY_PORT}"
+    export HTTP_PROXY="${DOCKER_CONTAINER_PROXY_PROTOCOL}://${DOCKER_CONTAINER_PROXY_HOST}:${DOCKER_CONTAINER_PROXY_PORT}"
+    export HTTPS_PROXY="${DOCKER_CONTAINER_PROXY_PROTOCOL}://${DOCKER_CONTAINER_PROXY_HOST}:${DOCKER_CONTAINER_PROXY_PORT}"
+
+    # 2. Setting proxy for apt
+    echo "Setting up proxy for apt ...";
+    envsubst "$DOCKER_CONTAINER_PROXY_PROTOCOL:$DOCKER_CONTAINER_PROXY_HOST:$DOCKER_CONTAINER_PROXY_PORT" < /tmp/apt_template.conf > /etc/apt/apt.conf
+
+    # 3. Setting proxy for curl
+    echo "Setting up proxy for curl ...";
+    envsubst "$DOCKER_CONTAINER_PROXY_PROTOCOL:$DOCKER_CONTAINER_PROXY_HOST:$DOCKER_CONTAINER_PROXY_PORT" < /tmp/.curlrc_template > ~/.curlrc
 fi
